e3b2dac96bd822a4b6ff482f960886af4d0ae4b9
MahdiHaeri <mahdihaerim@gmail.com>
2023-12-16 13:57:46 +0330

    Fix naming wire problem

--- MUX/mux_2_to_1.v
+++ MUX/mux_2_to_1.v
@@ -1,4 +1,4 @@
-module mux_2_to_1 (input a, b, sel, output y);
+module mux_2_to_1 (input a, b, sel, output out);
     // gate-level implementation
     wire sel_not;
     wire a_and_sel_not;
@@ -9,8 +9,8 @@ module mux_2_to_1 (input a, b, sel, output y);
     and(a_and_sel_not, a, sel_not);
     and(b_and_sel, b, sel);
 
-    or(y, a_and_sel_not, b_and_sel);
+    or(out, a_and_sel_not, b_and_sel);
 
     // dataflow implementation
-    // assign y = (sel == 1'b0) ? a : b;
+    // assign out = (sel == 1'b0) ? a : b;
 endmodule
--- MUX/mux_2_to_1_tb.v
+++ MUX/mux_2_to_1_tb.v
@@ -1,14 +1,14 @@
 module mux_2_to_1_tb;
 
     reg a, b, sel;
-    wire y;
+    wire out;
 
-    mux_2_to_1 uut(a, b, sel, y);
+    mux_2_to_1 uut(a, b, sel, out);
 
     initial begin
         $dumpfile("mux_2_to_1_tb.vcd");
         $dumpvars(0, mux_2_to_1_tb);
-        $monitor("a=%b, b=%b, sel=%b, y=%b", a, b, sel, y);
+        $monitor("a=%b, b=%b, sel=%b, out=%b", a, b, sel, out);
 
         a = 0;
         b = 0;
