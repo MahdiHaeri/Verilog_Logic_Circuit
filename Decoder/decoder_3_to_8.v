0a1308f5723c2c5b5a66e8d5dbb41ca8d4fc808d
MahdiHaeri <mahdihaerim@gmail.com>
2023-12-18 11:00:24 +0330

    Update decoder_3_to_8

--- Decoder/decoder_3_to_8.v
+++ Decoder/decoder_3_to_8.v
@@ -11,19 +11,19 @@ module decoder_3_to_8 (input wire enable, input wire [2:0] in, output wire [7:0]
 
     // decoder_2_to_4 dut1 (
     //     .enable(enable_1),
-    //     .in({in[1], in[0]}),
-    //     .out({out[3], out[2], out[1], out[0]})
+    //     .in(in[1:0]),
+    //     .out(out[3:0])
     // );
 
     // decoder_2_to_4 dut2 (
     //     .enable(enable_2),
-    //     .in({in[1], in[0]}),
-    //     .out({out[7], out[6], out[5], out[4]})
+    //     .in(in[1:0]),
+    //     .out(out[7:4])
     // );
 
     // ------------------------------------------------
 
-    dataflow implementation
+    // dataflow implementation
     decoder_2_to_4 dut1 (
         .enable(enable | in[2]),
         .in(in[1:0]),
