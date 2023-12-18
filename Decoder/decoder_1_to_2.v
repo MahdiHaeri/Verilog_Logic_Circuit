accde43c0e1128548f40eed258dc4e24bdba1847
MahdiHaeri <mahdihaerim@gmail.com>
2023-12-18 09:57:04 +0330

    Add data flow and behavioral implementation

--- Decoder/decoder_1_to_2.v
+++ Decoder/decoder_1_to_2.v
@@ -1,12 +1,37 @@
-module decoder_1_to_2 (input wire enable, input wire in, output wire out0, output wire out1);
+// active low enable and output decoder
+module decoder_1_to_2 (input wire enable, in, output wire out0, out1);
     // gate-level implementation
-    wire in_not;
-    wire enable_not;
+    // wire in_not;
+    // wire enable_not;
 
-    not(in_not, in);
-    not(enable_not, enable);
+    // not(in_not, in);
+    // not(enable_not, enable);
 
-    nand(out0, enable_not, in_not);
-    nand(out1, enable_not, in);
+    // nand(out0, enable_not, in_not);
+    // nand(out1, enable_not, in);
+
+    // ------------------------------------------------
+
+    // dataflow implementation
+    assign out0 = ~(~enable & ~in);
+    assign out1 = ~(~enable & in);
+
+    // ------------------------------------------------
+
+    // behavioral implementation --> for behavioral implementation, we need to change wire out0, out1 to reg out0, out1
+    // always @ (enable or in) begin
+    //     if (enable) begin 
+    //         out0 = 1;
+    //         out1 = 1;
+    //     end else begin
+    //         if (in) begin
+    //             out0 = 1;
+    //             out1 = 0;
+    //         end else begin
+    //             out0 = 0;
+    //             out1 = 1;
+    //         end
+    //     end
+    // end
 
 endmodule
