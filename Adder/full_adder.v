64dad7c27cee8bf996d719fbe9c6a73ad7ba2ab2
MahdiHaeri <mahdihaerim@gmail.com>
2023-12-18 20:03:30 +0330

    Add dataflow implementation of full_adder

--- Adder/full_adder.v
+++ Adder/full_adder.v
@@ -1,8 +1,16 @@
 module full_adder(input a, b, cin, output sum, cout);
-    wire s1, c1, c2;
 
-    half_adder ha1(a, b, s1, c1);
-    half_adder ha2(s1, cin, sum, c2);
-    or(cout, c1, c2);
+    // gate-level implementation
+    // wire s1, c1, c2;
+
+    // half_adder ha1(a, b, s1, c1);
+    // half_adder ha2(s1, cin, sum, c2);
+    // or(cout, c1, c2);
+
+    // -----------------------------------------------------
+
+    // dataflow implementation
+    assign sum = a ^ b ^ cin;
+    assign cout = (a & b) | (b & cin) | (a & cin);
 
 endmodule
