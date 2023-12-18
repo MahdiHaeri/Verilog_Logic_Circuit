8c22c0f8393c7741835e5e9a2af57533bf01252e
MahdiHaeri <mahdihaerim@gmail.com>
2023-12-18 17:54:07 +0330

    Add dataflow and behavioral implementation of mux_2_to_1

--- MUX/mux_2_to_1.v
+++ MUX/mux_2_to_1.v
@@ -1,16 +1,30 @@
-module mux_2_to_1 (input a, b, sel, output out);
+module mux_2_to_1 (input wire a, b, sel, output wire out);
     // gate-level implementation
-    wire sel_not;
-    wire a_and_sel_not;
-    wire b_and_sel;
+    // wire sel_not;
+    // wire a_and_sel_not;
+    // wire b_and_sel;
 
-    not(sel_not, sel);
+    // not(sel_not, sel);
 
-    and(a_and_sel_not, a, sel_not);
-    and(b_and_sel, b, sel);
+    // and(a_and_sel_not, a, sel_not);
+    // and(b_and_sel, b, sel);
 
-    or(out, a_and_sel_not, b_and_sel);
+    // or(out, a_and_sel_not, b_and_sel);
+
+    // ------------------------------------------------
 
     // dataflow implementation
-    // assign out = (sel == 1'b0) ? a : b;
+    assign out = (sel) ? b : a;
+
+    // ------------------------------------------------
+
+    // behavioral implementation --> output should be reg
+
+    // always @(*) begin
+    //     if (sel) begin
+    //         out = b;
+    //     end else begin
+    //         out = a;
+    //     end
+    // end
 endmodule
